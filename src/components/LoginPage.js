4bffbc3f2b04d11cd817d43cb6c83111b48c509d
amnonbb <amnonbb@gmail.com>
2018-03-31 14:01:51 +0300

    show welcome user name with profile options

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {client,BASE_URL} from '../tools/UserManager';
-import { Container,Message,Button,Divider } from 'semantic-ui-react';
+import { Container,Message,Button,Divider,Dropdown } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import '../App.css';
 
@@ -31,7 +31,14 @@ class LoginPage extends Component {
   render() {
 
       let login = (<Button size='massive' primary onClick={this.getUser} {...this.props} >Login</Button>);
-      let logout = (<Button size='mini' primary onClick={() => client.signoutRedirect()}>LogOut</Button>);
+      //let logout = (<Button size='mini' primary onClick={() => client.signoutRedirect()}>LogOut</Button>);
+      let user_dropdown = (
+          <Dropdown inline text=''>
+          <Dropdown.Menu>
+              <Dropdown.Item text='Settings' />
+              <Dropdown.Item text='Logout' onClick={() => client.signoutRedirect()} />
+          </Dropdown.Menu>
+          </Dropdown>);
       let main = (
             <Button.Group size='massive' >
                 <Button positive value='new' onClick={this.handleInsert}>&nbsp;&nbsp;Insert&nbsp;</Button>
@@ -42,14 +49,14 @@ class LoginPage extends Component {
 
     return (
           <Container textAlign='center' >
-          <Message size='massive' {...this.props} >
-              <Message.Header >
-                  Insert Archive
+          <Message size='massive'>
+              <Message.Header {...this.props}>
+                  {this.props.user === null ? "Insert Archive" : "Welcome, "+this.props.user.name}
+                  {this.props.user === null ? "" : user_dropdown}
               </Message.Header>
               <p>Service for inserting new materials into the bb archive.</p>
               {this.props.user === null ? login : main}
           </Message>
-              {this.props.user === null ? "" : logout}
           </Container>
     );
   }
